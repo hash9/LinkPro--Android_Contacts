7955adaadb2dcfb1a431030017f425f639ae1f57
hash9 <hprajapati9495@gmail.com>
2017-01-28 19:21:03 +0530

    add

--- app/src/main/java/com/example/dell/contacts/StudentFormActivity.java
+++ app/src/main/java/com/example/dell/contacts/StudentFormActivity.java
@@ -1,11 +1,13 @@
 package com.example.dell.contacts;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,7 +18,6 @@ public class StudentFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_form);
-
 
         if(hasIntentionToUpdate()) {
             Student student = getOriginalStudentToUpdate();
